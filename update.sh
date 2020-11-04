b6032ba406f24380f178c15015696a9341b2ef33
zaid gharaybeh <z.gharaybeh@gmail.com>
2020-11-04 10:05:04 -0800

    f

--- update.sh
+++ update.sh
@@ -1,13 +1,11 @@
+mkdir -p ~/dev/
+cd ~/dev
+rm -rf .dotfiles
+git clone https://github.com/zaid-g/.dotfiles.git
+cd .dotfiles
 touch -a ~/.zshrc
 touch -a ~/.tmux.conf
 touch -a ~/.vimrc
 grep -qxF 'source ~/dev/.dotfiles/.vimrc' ~/.vimrc || echo 'source ~/dev/.dotfiles/.vimrc' >> ~/.vimrc
 grep -qxF '. ~/dev/.dotfiles/.zshrc' ~/.zshrc || echo '. ~/dev/.dotfiles/.zshrc' >> ~/.zshrc
 grep -qxF 'source-file ~/dev/.dotfiles/.tmux.conf' ~/.tmux.conf || echo 'source-file ~/dev/.dotfiles/.tmux.conf' >> ~/.tmux.conf
-mkdir -p ~/dev/
-cd ~/dev
-rm -rf .dotfiles
-git clone https://github.com/zaid-g/.dotfiles.git
-cd .dotfiles
-
-
