ad265ca670cdb7b774a1054dcc13e9b5318823f8
zaid gharaybeh <z.gharaybeh@gmail.com>
2021-03-08 14:00:20 -0800

    a

--- plugins-vim-install.sh
+++ plugins-vim-install.sh
@@ -2,3 +2,5 @@ mkdir -p ~/.vim/pack/plugins/start
 cd ~/.vim/pack/plugins/start
 git clone https://github.com/jpalardy/vim-slime.git
 git clone https://github.com/simeji/winresizer.git
+mkdir -p ~/.vim/pack/git-plugins/start
+git clone --depth 1 https://github.com/dense-analysis/ale.git ~/.vim/pack/git-plugins/start/ale
