071d3df153b8e691dc7829e773ce6ff853aa94cf
zaid gharaybeh <33379149+zaid-g@users.noreply.github.com>
2021-06-13 15:06:26 -0700

    Update plugins-vim-install.sh

--- plugins-vim-install.sh
+++ plugins-vim-install.sh
@@ -4,3 +4,6 @@ git clone https://github.com/jpalardy/vim-slime.git
 git clone https://github.com/simeji/winresizer.git
 git clone https://github.com/doums/darcula.git
 git clone --depth 1 https://github.com/dense-analysis/ale.git
+git clone https://github.com/SirVer/ultisnips.git
+git clone git://github.com/honza/vim-snippets.git
+grep -qxF 'colo darcula' ~/.vimrc || echo "$(cat ~/.vimrc; printf 'colo darcula')" > ~/.vimrc
