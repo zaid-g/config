ef9d42ab46966bb4c014d948a19746e4fe5ed583
ubuntu <z.gharaybeh@gmail.com>
2021-06-26 12:17:30 -0700

    f

--- plugins-vim-install.sh
+++ plugins-vim-install.sh
@@ -6,4 +6,4 @@ git clone https://github.com/doums/darcula.git
 git clone --depth 1 https://github.com/dense-analysis/ale.git
 git clone https://github.com/SirVer/ultisnips.git
 git clone git://github.com/honza/vim-snippets.git
-grep -qxF 'colo darcula' ~/.vimrc || echo "$(cat ~/.vimrc; printf 'colo darcula')" > ~/.vimrc
+grep -qxF 'colo darcula' ~/.vimrc || echo "$(cat ~/.vimrc; printf '\ncolo darcula\n')" > ~/.vimrc
