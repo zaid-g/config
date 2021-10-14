99d149304dcbe19eaad216c404e86603dfe2fe72
ubuntu <z.gharaybeh@gmail.com>
2021-10-13 18:34:15 -0700

    f

--- plugins-vim-install.sh
+++ plugins-vim-install.sh
@@ -4,14 +4,13 @@ cd ~/.vim/pack/myplugins/start
 git clone https://github.com/jpalardy/vim-slime.git
 git clone https://github.com/simeji/winresizer.git
 git clone https://github.com/doums/darcula.git
-git clone --depth 1 https://github.com/dense-analysis/ale.git
+git clone https://github.com/dense-analysis/ale.git
 git clone https://github.com/SirVer/ultisnips.git
 git clone git://github.com/honza/vim-snippets.git
-grep -qxF 'colo darcula' ~/.vimrc || echo "$(cat ~/.vimrc; printf '\ncolo darcula\n')" > ~/.vimrc
-cd ~/env/dotfiles
 
 rm -rf ~/.local/share/nvim/site/pack/myplugins/start
 mkdir -p ~/.local/share/nvim/site/pack/myplugins/start
-cp -r ~/.vim/pack/myplugins/start/* ~/.local/share/nvim/site/pack/myplugins/start/
-grep -qxF 'colo darcula' ~/.config/nvim/init.vim || echo "$(cat ~/.config/nvim/init.vim; printf '\ncolo darcula\n')" > ~/.config/nvim/init.vim
+cd ~/.local/share/nvim/site/pack/myplugins/start
+cp -r ~/.vim/pack/myplugins/start/* .
+git clone https://github.com/neovim/nvim-lspconfig.git
 cd ~/env/dotfiles
