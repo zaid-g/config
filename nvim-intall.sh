0b84dfc316024e658b01483d51fda7e6315d2ddb
ubuntu <z.gharaybeh@gmail.com>
2022-01-10 08:27:44 -0800

    f

--- nvim-intall.sh
+++ nvim-intall.sh
@@ -9,10 +9,10 @@ nvim_appimage_checksum_link=$nvim_latest_github_path/nvim.appimage.sha256sum
 nvim_linux64_link=$nvim_latest_github_path/nvim-linux64.tar.gz
 nvim_linux64_checksum_link=$nvim_latest_github_path/nvim-linux64.tar.gz.sha256sum
 # download
-wget nvim_appimage_link
-wget nvim_appimage_checksum_link
-wget nvim_linux64_link
-wget nvim_linux64_checksum_link
+wget $nvim_appimage_link
+wget $nvim_appimage_checksum_link
+wget $nvim_linux64_link
+wget $nvim_linux64_checksum_link
 # check shasums
 sha256sum -c nvim.appimage.sha256sum
 sha256sum -c nvim-linux64.tar.gz.sha256sum
