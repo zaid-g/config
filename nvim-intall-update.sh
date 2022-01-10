cc7c70594e37e38228f1e321ebab526ab20182cb
ubuntu <z.gharaybeh@gmail.com>
2022-01-10 08:47:30 -0800

    f

--- nvim-intall-update.sh
+++ nvim-intall-update.sh
@@ -20,5 +20,4 @@ wget $nvim_linux64_checksum_link
 sha256sum -c nvim.appimage.sha256sum
 sha256sum -c nvim-linux64.tar.gz.sha256sum
 # decompress the tar linux code
-tar -zxvf nvim-link.tar.gz
-
+tar -zxvf nvim-linux64.tar.gz
