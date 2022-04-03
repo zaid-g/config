d2e2d6692bb09c0385a9dd91236430f01c9c5a0e
ubuntu <z.gharaybeh@gmail.com>
2022-04-03 16:03:17 -0700

    a

--- scripts/OS_setup/ubuntu.sh
+++ scripts/OS_setup/ubuntu.sh
@@ -13,7 +13,7 @@ pip3 install -r ./python-packages-install.txt
 echo 'PS1="%B%{$fg[green]%}[%{$fg[green]%}%n%{$fg[green]%}@%{$fg[green]%}%M %{$fg[green]%}%~%{$fg[green]%}]%{$reset_color%}$%b "' >> ~/.zshrc
 echo '. ~/.xinitrc' >> ~/.zshrc
 
-sudo apt install zsh python3-pip python3-venv vim ruby-dev ruby-bundler wget curl golang tmux postgresql postgresql-contrib xclip -y
+sudo apt install zsh python3-pip python3-venv vim ruby-dev ruby-bundler wget curl golang tmux postgresql postgresql-contrib xclip htop -y
 sudo apt-get install texlive-full -y --fix-missing
 curl -sL https://deb.nodesource.com/setup_14.x | sudo -E bash -
 sudo apt install nodejs -y
@@ -24,3 +24,15 @@ chsh -s /usr/bin/zsh
 
 # change resolution 1600x900
 # change keyboard repeat keys delay and speed 200, 50
+
+# install discord and snap
+sudo apt install snapd -y
+#reboot
+sudo snap install core
+sudo snap install discord
+# to update
+sudo snap refresh
+# to run
+snap run discord
+# to remove
+sudo snap remove discord
