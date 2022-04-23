9a81ecf08102e9eb45f3fc9692e259aca86e029e
debian11 <debian11@debian-BULLSEYE-live-builder-AMD64>
2022-04-23 12:37:40 -0700

    f

--- scripts/OS_setup/ubuntu.sh
+++ scripts/OS_setup/ubuntu.sh
@@ -6,14 +6,14 @@ mkdir ~/dev
 cd ~/dev
 git clone https://github.com/zaid-g/environment.git
 cd ~/dev/environment
-. ./update.sh
-. ./nvim-install-update.sh
-. ./nvim-plugins-install-update.sh
-pip3 install -r ./python-packages-install.txt
+. ./scripts/update.sh
+. ./scripts/nvim-install-update.sh
+. ./scripts/nvim-plugins-install-update.sh
+pip3 install -r ./scripts/python-packages-install.txt
 echo 'PS1="%B%{$fg[green]%}[%{$fg[green]%}%n%{$fg[green]%}@%{$fg[green]%}%M %{$fg[green]%}%~%{$fg[green]%}]%{$reset_color%}$%b "' >> ~/.zshrc
 echo '. ~/.xinitrc' >> ~/.zshrc
 
-sudo apt install zsh python3-pip python3-venv vim ruby-dev ruby-bundler wget curl golang tmux postgresql postgresql-contrib xclip htop -y
+sudo apt install zsh python3-pip python3-venv ruby-dev ruby-bundler wget curl golang tmux postgresql postgresql-contrib xclip htop -y
 sudo apt-get install texlive-full -y --fix-missing
 curl -sL https://deb.nodesource.com/setup_14.x | sudo -E bash -
 sudo apt install nodejs -y
