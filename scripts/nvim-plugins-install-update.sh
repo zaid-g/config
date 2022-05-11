912b52ad2b4e930da25af2815218dc16d93b7e0a
debian11 <debian11@debian-BULLSEYE-live-builder-AMD64>
2022-05-11 16:37:26 -0700

    fix

--- scripts/nvim-plugins-install-update.sh
+++ scripts/nvim-plugins-install-update.sh
@@ -5,6 +5,6 @@ git clone https://github.com/jpalardy/vim-slime.git
 git clone https://github.com/simeji/winresizer.git
 git clone https://github.com/doums/darcula.git
 git clone https://github.com/SirVer/ultisnips.git
-git clone git://github.com/honza/vim-snippets.git
+git clone https://github.com/honza/vim-snippets.git
 git clone https://github.com/neovim/nvim-lspconfig.git
 cd ~/dev/environment/scripts
