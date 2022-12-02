a6b81f82391f12bff3a2f8752f097a1a43ab674f
debian11 <debian11@debian-BULLSEYE-live-builder-AMD64>
2022-12-01 21:44:14 -0800

    f

--- init.lua
+++ init.lua
@@ -1,56 +1,53 @@
-vim.lsp.set_log_level("debug")
+-- Mappings.
+-- See `:help vim.diagnostic.*` for documentation on any of the below functions
+local opts = { noremap=true, silent=true }
+vim.keymap.set('n', 'md', vim.diagnostic.open_float, opts)
+vim.keymap.set('n', '[d', vim.diagnostic.goto_prev, opts)
+vim.keymap.set('n', ']d', vim.diagnostic.goto_next, opts)
+vim.keymap.set('n', 'mq', vim.diagnostic.setloclist, opts)
 
 -- Use an on_attach function to only map the following keys
 -- after the language server attaches to the current buffer
 local on_attach = function(client, bufnr)
-  local function buf_set_keymap(...) vim.api.nvim_buf_set_keymap(bufnr, ...) end
-  local function buf_set_option(...) vim.api.nvim_buf_set_option(bufnr, ...) end
-
   -- Enable completion triggered by <c-x><c-o>
-  buf_set_option('omnifunc', 'v:lua.vim.lsp.omnifunc')
+  vim.api.nvim_buf_set_option(bufnr, 'omnifunc', 'v:lua.vim.lsp.omnifunc')
 
   -- Mappings.
-  local opts = { noremap=true, silent=true }
-
   -- See `:help vim.lsp.*` for documentation on any of the below functions
-  buf_set_keymap('n', 'gD', '<cmd>lua vim.lsp.buf.declaration()<CR>', opts)
-  buf_set_keymap('n', 'gd', '<cmd>lua vim.lsp.buf.definition()<CR>', opts)
-  buf_set_keymap('n', 'mh', '<cmd>lua vim.lsp.buf.hover()<CR>', opts)
-  buf_set_keymap('n', 'mgi', '<cmd>lua vim.lsp.buf.implementation()<CR>', opts)
-  -- buf_set_keymap('n', '<C-k>', '<cmd>lua vim.lsp.buf.signature_help()<CR>', opts)
-  -- buf_set_keymap('n', '<space>wa', '<cmd>lua vim.lsp.buf.add_workspace_folder()<CR>', opts)
-  -- buf_set_keymap('n', '<space>wr', '<cmd>lua vim.lsp.buf.remove_workspace_folder()<CR>', opts)
-  -- buf_set_keymap('n', '<space>wl', '<cmd>lua print(vim.inspect(vim.lsp.buf.list_workspace_folders()))<CR>', opts)
-  -- buf_set_keymap('n', '<space>D', '<cmd>lua vim.lsp.buf.type_definition()<CR>', opts)
-  buf_set_keymap('n', 'mrn', '<cmd>lua vim.lsp.buf.rename()<CR>', opts)
-  buf_set_keymap('n', 'mca', '<cmd>lua vim.lsp.buf.code_action()<CR>', opts)
-  buf_set_keymap('n', 'gr', '<cmd>lua vim.lsp.buf.references()<CR>', opts)
-  buf_set_keymap('n', 'md', '<cmd>lua vim.lsp.diagnostic.show_line_diagnostics()<CR>', opts)
-  buf_set_keymap('n', '[d', '<cmd>lua vim.lsp.diagnostic.goto_prev()<CR>', opts)
-  buf_set_keymap('n', ']d', '<cmd>lua vim.lsp.diagnostic.goto_next()<CR>', opts)
-  buf_set_keymap('n', 'mq', '<cmd>lua vim.lsp.diagnostic.set_loclist()<CR>', opts)
-  buf_set_keymap('n', 'mf', '<cmd>lua vim.lsp.buf.formatting()<CR>', opts)
+  local bufopts = { noremap=true, silent=true, buffer=bufnr }
+  vim.keymap.set('n', 'gD', vim.lsp.buf.declaration, bufopts)
+  vim.keymap.set('n', 'gd', vim.lsp.buf.definition, bufopts)
+  vim.keymap.set('n', 'mh', vim.lsp.buf.hover, bufopts)
+  vim.keymap.set('n', 'gi', vim.lsp.buf.implementation, bufopts)
+  -- vim.keymap.set('n', '<C-k>', vim.lsp.buf.signature_help, bufopts)
+  -- vim.keymap.set('n', '<space>wa', vim.lsp.buf.add_workspace_folder, bufopts)
+  -- vim.keymap.set('n', '<space>wr', vim.lsp.buf.remove_workspace_folder, bufopts)
+  -- vim.keymap.set('n', '<space>wl', function()
+  --   print(vim.inspect(vim.lsp.buf.list_workspace_folders()))
+  -- end, bufopts)
+  vim.keymap.set('n', 'mtd', vim.lsp.buf.type_definition, bufopts)
+  vim.keymap.set('n', 'mrn', vim.lsp.buf.rename, bufopts)
+  vim.keymap.set('n', 'mca', vim.lsp.buf.code_action, bufopts)
+  vim.keymap.set('n', 'gr', vim.lsp.buf.references, bufopts)
+  vim.keymap.set('n', 'mf', function() vim.lsp.buf.format { async = true } end, bufopts)
 end
 
-
-local nvim_lsp = require('lspconfig')
-
+local lsp_flags = {
+  -- This is the default in Nvim 0.7+
+  debounce_text_changes = 150,
+}
 
 -- pyright. check out OS install script
-nvim_lsp.pyright.setup{
+require('lspconfig')['pyright'].setup{
     on_attach = on_attach,
-    flags = {
-      debounce_text_changes = 150,
-    }
+    flags = lsp_flags
 }
 
 
 -- efm. check out OS install script
-nvim_lsp.efm.setup {
+require('lspconfig')['efm'].setup {
     on_attach = on_attach,
-    flags = {
-      debounce_text_changes = 150,
-    },
+    flags = lsp_flags,
     init_options = {documentFormatting = true},
     filetypes = {"python", "lua"},
     settings = {
@@ -67,11 +64,9 @@ nvim_lsp.efm.setup {
 }
 
 -- jsonls. `npm i -g vscode-langservers-extracted`
-nvim_lsp.jsonls.setup{
+require('lspconfig')['jsonls'].setup{
     on_attach = on_attach,
-    flags = {
-      debounce_text_changes = 150,
-    },
+    flags = lsp_flags,
     init_options = {documentFormatting = true},
     filetypes = {"json", "jsonc"},
     settings = {
@@ -80,11 +75,9 @@ nvim_lsp.jsonls.setup{
 }
 
 -- ccls (for c/c++). `brew install ccls`
-nvim_lsp.ccls.setup {
+require('lspconfig')['ccls'].setup {
     on_attach = on_attach,
-    flags = {
-      debounce_text_changes = 150,
-    },
+    flags = lsp_flags,
     init_options = {
         documentFormatting = true,
         compilationDatabaseDirectory = "build";
