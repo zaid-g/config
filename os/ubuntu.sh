f329085b9644da7fd2c7548f4b6df185392db596
z g <z.gharaybeh@gmail.com>
2023-01-08 13:41:22 -0800

    f

--- os/ubuntu.sh
+++ os/ubuntu.sh
@@ -1,25 +1,25 @@
 #!/bin/bash
 # run this file without sudo
 
+set -e # break script if any command fails after this line
+
 # for sudo
 sudo touch ~/misc
 
-# environment
-sudo apt install git python3 python3-venv python3-pip -y
+## packages & setup environment
+sudo apt install git python3 python3-venv python3-pip zsh wget curl golang tmux htop xournal ccls -y
 . ~/doc/projects/config/vim/nvim-install-update.sh
 . ~/doc/projects/config/vim/nvim-plugins-install-update.sh
-. ~/doc/projects/config/fetch_and_apply_config.sh
-
-set -e # break script if any command fails after this line
-
-sudo chsh -s $(which zsh) $(whoami)
-
+. ~/doc/projects/config/apply_config.sh
+# get neovim and basic python libraries
 pip3 install -r ~/doc/projects/config/python/python-packages-install.txt
 
+## zsh
+# default shell
+sudo chsh -s $(which zsh) $(whoami)
 echo 'PS1="%B%{$fg[green]%}[%{$fg[green]%}%n%{$fg[green]%}@%{$fg[green]%}%M %{$fg[green]%}%~%{$fg[green]%}]%{$reset_color%}$%b "' >> ~/.zshrc
-echo '. ~/.xinitrc' >> ~/.zshrc
 
-sudo apt install zsh wget curl golang tmux htop xournal ccls -y
+# Language servers
 curl -sL https://deb.nodesource.com/setup_14.x | sudo -E bash -
 sudo apt install nodejs -y
 sudo npm i -g pyright
@@ -27,10 +27,8 @@ sudo npm i -g vscode-langservers-extracted
 GO111MODULE=on go get github.com/mattn/efm-langserver@latest
 echo 'export PATH=$PATH:~/go/bin' >> ~/.zshrc
 
+# cleanup
 sudo apt autoremove
 
-sudo apt-get install texlive-full -y --fix-missing
-
-# change resolution 1600x900
-# change keyboard repeat keys delay and speed 225, 40
-# install discord
+# optional stuff
+sudo apt-get install texlive-full
