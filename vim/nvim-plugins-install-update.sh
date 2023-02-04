486385ddc565b8f9a940b22e9dbcd48728e6ed74
z g <z.gharaybeh@gmail.com>
2023-02-04 12:41:13 -0800

    stuffs

--- vim/nvim-plugins-install-update.sh
+++ vim/nvim-plugins-install-update.sh
@@ -1,11 +1,33 @@
-rm -rf ~/.local/share/nvim/site/pack/myplugins/start
-mkdir -p ~/.local/share/nvim/site/pack/myplugins/start
-cd ~/.local/share/nvim/site/pack/myplugins/start
+rm -rf ~/.local/share/nvim/site/pack/downloaded/start
+mkdir -p ~/.local/share/nvim/site/pack/downloaded/start
+cd ~/.local/share/nvim/site/pack/downloaded/start
+
+# send to tmux plugin
 git clone https://github.com/jpalardy/vim-slime.git
+
+# resize windows
 git clone https://github.com/simeji/winresizer.git
-git clone https://github.com/doums/darcula.git
-git clone https://github.com/SirVer/ultisnips.git
-git clone https://github.com/honza/vim-snippets.git
-git clone https://github.com/neovim/nvim-lspconfig.git
+
+# :Rename, :Delete file commands without buffer complications
 git clone https://github.com/tpope/vim-eunuch.git
-git clone https://github.com/aklt/plantuml-syntax
+
+## Lsp stuff
+git clone https://github.com/neovim/nvim-lspconfig.git
+# completion stuff including snippets
+git clone https://github.com/hrsh7th/nvim-cmp.git
+git clone https://github.com/hrsh7th/cmp-nvim-lsp.git
+git clone https://github.com/L3MON4D3/LuaSnip.git
+git clone https://github.com/saadparwaiz1/cmp_luasnip.git
+# abstract syntax tree
+git clone https://github.com/nvim-treesitter/nvim-treesitter.git
+git clone https://github.com/nvim-treesitter/nvim-treesitter-textobjects.git
+
+# indent blankline
+git clone https://github.com/lukas-reineke/indent-blankline.nvim.git
+
+# code comment plugin
+git clone https://github.com/numToStr/Comment.nvim.git
+
+# fuzzy finder
+git clone https://github.com/nvim-telescope/telescope.nvim.git
+git clone https://github.com/nvim-lua/plenary.nvim.git # idk why/if this is needed
