62c91969f3677149c4678b6ad40f08bfe5d48119
z g <z.gharaybeh@gmail.com>
2023-02-14 19:58:33 -0800

    f

--- os/ubuntu_wsl.sh
+++ os/ubuntu_wsl.sh
@@ -1,16 +1,14 @@
 #!/bin/bash
 # run this file without sudo
 
+## Notes:
+# for remapping capslock, download PowerToys
+# ---------------
+
 set -e # break script if any command fails after this line
 
 # for sudo
 sudo echo "Running ..."
-
-## keyboard settings
-# swap capslock with escape
-dconf write "/org/gnome/desktop/input-sources/xkb-options" "[ 'caps:swapescape']"
-gsettings set org.gnome.desktop.peripherals.keyboard repeat-interval 20
-gsettings set org.gnome.desktop.peripherals.keyboard delay 250
 
 ## packages & setup environment
 sudo apt update -y
