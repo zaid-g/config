35722bfffd73aad6210eb0abaccdf2ae4e28d6ff
z g <z.gharaybeh@gmail.com>
2025-11-13 22:50:29 -0800

    F

--- git/setup.sh
+++ git/setup.sh
@@ -6,3 +6,7 @@ git config --global delta.plus-style "syntax #004400"
 git config --global delta.line-numbers true
 git config --global delta.line-numbers-left-format "{nm:>4}┊"
 git config --global delta.line-numbers-right-format "{np:>4}│"
+git config --global delta.word-diff-regex "\\w+|[^\\w\\s]+"
+git config --global delta.minus-emph-style "syntax #ff0000"
+git config --global delta.plus-emph-style "syntax #008800"
+git config --global delta.whitespace-error-style "reverse purple"
