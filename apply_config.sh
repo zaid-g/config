194b7316f10e7668b28349da7dddecb359626617
z g <z.gharaybeh@gmail.com>
2025-12-31 12:36:31 -0800

    F

--- apply_config.sh
+++ apply_config.sh
@@ -15,41 +15,41 @@ mkdir -p ~/.ipython/profile_default
 # %% -------- [nvim] ----------:
 
 # touch -a ~/.config/nvim/init.lua
-# grep -qF 'dofile(vim.fn.expand("~/doc/config/vi/nvim/basic.lua"))' ~/.config/nvim/init.lua || echo "$(
+# grep -qF 'dofile(vim.fn.expand("~/doc/config/config/vi/nvim/basic.lua"))' ~/.config/nvim/init.lua || echo "$(
 #     cat ~/.config/nvim/init.lua
-#     printf '%s\n' 'dofile(vim.fn.expand("~/doc/config/vi/nvim/basic.lua"))'
+#     printf '%s\n' 'dofile(vim.fn.expand("~/doc/config/config/vi/nvim/basic.lua"))'
 # )" >~/.config/nvim/init.lua
-# grep -qF 'dofile(vim.fn.expand("~/doc/config/vi/nvim/advanced.lua"))' ~/.config/nvim/init.lua || echo "$(
+# grep -qF 'dofile(vim.fn.expand("~/doc/config/config/vi/nvim/advanced.lua"))' ~/.config/nvim/init.lua || echo "$(
 #     cat ~/.config/nvim/init.lua
-#     printf '%s\n' '--dofile(vim.fn.expand("~/doc/config/vi/nvim/advanced.lua"))'
+#     printf '%s\n' '--dofile(vim.fn.expand("~/doc/config/config/vi/nvim/advanced.lua"))'
 # )" >~/.config/nvim/init.lua
 
 # %% -------- [zsh] ----------:
 
 touch -a ~/.zshrc
-grep -qF '. ~/doc/config/zsh/zshrc' ~/.zshrc || echo "$(
-    printf '. ~/doc/config/zsh/zshrc\n'
+grep -qF '. ~/doc/config/config/zsh/zshrc' ~/.zshrc || echo "$(
+    printf '. ~/doc/config/config/zsh/zshrc\n'
     cat ~/.zshrc
 )" >~/.zshrc
 
 # %% -------- [tmux] ----------:
 
 touch -a ~/.tmux.conf
-grep -qF 'source-file ~/doc/config/tmux/tmux.conf' ~/.tmux.conf || echo "$(
-    printf 'source-file ~/doc/config/tmux/tmux.conf\n'
+grep -qF 'source-file ~/doc/config/config/tmux/tmux.conf' ~/.tmux.conf || echo "$(
+    printf 'source-file ~/doc/config/config/tmux/tmux.conf\n'
     cat ~/.tmux.conf
 )" >~/.tmux.conf
 
 # %% -------- [python] ----------:
 
 # pycodestyle
-cp ~/doc/config/python/pycodestyle ~/.config/
+cp ~/doc/config/config/python/pycodestyle ~/.config/
 # ipython
-cp ~/doc/config/python/ipython_config.py ~/.ipython/profile_default/
+cp ~/doc/config/config/python/ipython_config.py ~/.ipython/profile_default/
 
 # %% -------- [sway] ----------:
 
-cp ~/doc/config/sway/* ~/.config/sway/
+cp ~/doc/config/config/sway/* ~/.config/sway/
 chmod +x ~/.config/sway/clamshell.sh
 chmod +x ~/.config/sway/status.sh
 
@@ -60,19 +60,19 @@ grep -qF '[general]' ~/.config/alacritty/alacritty.toml || echo "$(
     cat ~/.config/alacritty/alacritty.toml
     printf '%s\n' '[general]'
 )" >~/.config/alacritty/alacritty.toml
-grep -qF 'import = ["~/doc/config/alacritty/alacritty.toml"]' ~/.config/alacritty/alacritty.toml || echo "$(
+grep -qF 'import = ["~/doc/config/config/alacritty/alacritty.toml"]' ~/.config/alacritty/alacritty.toml || echo "$(
     cat ~/.config/alacritty/alacritty.toml
-    printf '%s\n' 'import = ["~/doc/config/alacritty/alacritty.toml"]'
+    printf '%s\n' 'import = ["~/doc/config/config/alacritty/alacritty.toml"]'
 )" >~/.config/alacritty/alacritty.toml
 
 # %% -------- [git] ----------:
 
-bash ~/doc/config/git/setup.sh
+bash ~/doc/config/config/git/setup.sh
 
 # %% -------- [amazonq] ----------:
 
 mkdir -p ~/.aws/amazonq/cli-agents
-cp ~/doc/config/aws/amazonq/cli-agents/* ~/.aws/amazonq/cli-agents/
+cp ~/doc/config/config/aws/amazonq/cli-agents/* ~/.aws/amazonq/cli-agents/
 
 # %% -------- [done] ----------:
 
--- fetch.sh
+++ fetch.sh
@@ -3,4 +3,4 @@ cd ~/doc
 rm -rf config
 #clone new repo, try ssh first so if key exists u can push
 git clone git@github.com:zaid-g/config.git
-git clone https://github.com/zaid-g/config.git
+git clone https://github.com:zaid-g/config.git
