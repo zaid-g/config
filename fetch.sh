1dcf5ea94544e3d20ff80f5ed18ea3580fb85b87
z g <z.gharaybeh@gmail.com>
2026-02-11 07:27:52 -0800

    2026-02-11 07:27:52

--- fetch.sh
+++ fetch.sh
@@ -1,5 +1,3 @@
 cd ~/doc/config
-#remove old repo
-rm -rf config
-#clone new repo, try ssh first so if key exists u can push
-git clone git@github.com:zaid-g/config.git || git clone https://github.com/zaid-g/config.git
+#clone new repo to temp location, try ssh first so if key exists u can push
+(git clone git@github.com:zaid-g/config.git config.new || git clone https://github.com/zaid-g/config.git config.new) && rm -rf config && mv config.new config
