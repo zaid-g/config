fe91d2243413ffb2f42817fcea67f37ccb764288
z g <z.gharaybeh@gmail.com>
2026-02-10 20:41:36 -0800

    2026-02-10 20:41:36

---
+++ vi/lazyvim/install.sh
@@ -0,0 +1,9 @@
+# required
+mv ~/.config/lazyvim{,.bak}
+
+# optional but recommended
+mv ~/.local/share/lazyvim{,.bak}
+mv ~/.local/state/lazyvim{,.bak}
+mv ~/.cache/lazyvim{,.bak}
+
+git clone https://github.com/LazyVim/starter ~/.config/lazyvim
