d26eb378f9cb0f0f53a92b3d9f2feff4308382ca
z g <z.gharaybeh@gmail.com>
2026-02-11 08:10:04 -0800

    2026-02-11 08:10:04

--- fetch.sh
+++
@@ -1,3 +0,0 @@
-cd ~/doc/config
-#clone new repo to temp location, try ssh first so if key exists u can push
-(git clone git@github.com:zaid-g/config.git config.new || git clone https://github.com/zaid-g/config.git config.new) && rm -rf config && mv config.new config
--- fetch_and_apply_config.sh
+++
@@ -1,3 +0,0 @@
-cd ~  # to outside of config dir if inside
-bash ~/doc/config/config/fetch.sh
-bash ~/doc/config/config/apply_config.sh
